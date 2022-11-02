7f673c4a312e601d8538f5e06214b022f950155e
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2022-11-02 12:04:15 -0300

    Update Jantar.java

--- UdemyJava/src/classe/desafio/Jantar.java
+++ UdemyJava/src/classe/desafio/Jantar.java
@@ -8,15 +8,17 @@ public class Jantar {
 		
 		Comida c1 = new Comida();
 		
-		p1.nome="emanuel";
+		p1.nome="Emanuel ";
 		p1.peso=74;
 		
 		c1.nome="Feijao";
 		c1.peso=0.300;
 		
+		String peso = String.valueOf(p1.peso);
 		
+		System.out.println(p1.nome.concat(peso));
+		System.out.printf("O peso de %s antes de comer %s era %.2f e após comer é %.2f", p1.nome, c1.nome, p1.peso, p1.comer(c1.peso));
 		
-		System.out.println(p1.comer(c1.peso));
 		
 		
 		
