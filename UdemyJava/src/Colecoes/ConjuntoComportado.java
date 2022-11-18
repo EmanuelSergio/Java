f44b57dc8e413016fe7ef49fdfc9330d650bcd41
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2022-11-18 00:09:27 -0300

    Update ConjuntoComportado.java

--- UdemyJava/src/Colecoes/ConjuntoComportado.java
+++ UdemyJava/src/Colecoes/ConjuntoComportado.java
@@ -1,7 +1,10 @@
 package Colecoes;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class ConjuntoComportado {
 
@@ -12,14 +15,39 @@ public class ConjuntoComportado {
 		
 		//OU:
 		
-		Set<String> lista = new HashSet<>(); //quando eu coloco <String>, estou definindo que meu list aceitara apenas String
+		//Set<String> lista = new HashSet<>(); //quando eu coloco <String>, estou definindo que meu list aceitara apenas String
+		SortedSet<String> lista = new TreeSet<>(); // vai deixar a lista organizada
 		
 		lista.add("Emanuel");
 		lista.add("Lucas");
 		lista.add("Jão");
 		lista.add("Wendel");
 		lista.add("Maria");
+		lista.add("Ana");
 		
+		
+		for (String canditado : lista) {
+			System.out.println(canditado);
+			
+		}
+		
+		Set<Integer> numeros = new HashSet<>();
+		
+		numeros.add(5);
+		numeros.add(88);
+		numeros.add(4);
+		numeros.add(9);
+		numeros.add(10);
+		
+		int maior = 0;
+		
+		for (Integer integer : numeros) {
+			if (integer > maior) {
+				integer=maior;
+				System.out.println("eu sou o maior: " +maior);
+			}
+			System.out.println(integer);
+		}
 		
 		
 		
