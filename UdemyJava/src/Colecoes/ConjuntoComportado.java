bd70ad78d138ba36eb0c5881cecca42180374464
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2022-11-17 23:23:01 -0300

    Create ConjuntoComportado.java

---
+++ UdemyJava/src/Colecoes/ConjuntoComportado.java
@@ -0,0 +1,28 @@
+package Colecoes;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoComportado {
+
+	public static void main(String[] args) {
+		
+		
+		//Set<String> lista = new HashSet<String>();  
+		
+		//OU:
+		
+		Set<String> lista = new HashSet<>(); //quando eu coloco <String>, estou definindo que meu list aceitara apenas String
+		
+		lista.add("Emanuel");
+		lista.add("Lucas");
+		lista.add("Jão");
+		lista.add("Wendel");
+		lista.add("Maria");
+		
+		
+		
+		
+	}
+	
+}
