e1065e25ca1650cc79295acb6d98f1bacc871569
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2022-11-20 22:42:47 -0300

    Update Mapa.java

--- UdemyJava/src/Colecoes/Mapa.java
+++ UdemyJava/src/Colecoes/Mapa.java
@@ -2,6 +2,7 @@ package Colecoes;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class Mapa {
 
@@ -15,6 +16,9 @@ public class Mapa {
 		usuarios.put(3, "Emanuel");
 		usuarios.put(4, "Rebeca");
 		
+		//System.out.println(usuarios.remove(1));aqui se remove pela chave
+		System.out.println(usuarios.remove(3, "Emanuel"));
+		System.out.println(usuarios.remove(2, "Emanuel")); //vai retornar false pois a chave não combina com o valor
 		
 		System.out.println(usuarios.size());
 		System.out.println(usuarios.isEmpty());
@@ -23,9 +27,28 @@ public class Mapa {
 		System.out.println(usuarios.values());//mostra apenas os valores (String)
 		System.out.println(usuarios.entrySet());//mostra os valores junto com o codigo (Integer)
 		
+		System.out.println(usuarios.containsKey(3));//esse nao possui mais o contains por String (se indica o codigo para saber de contem)
+		System.out.println(usuarios.containsValue("Emanuel"));//esse retorna apenas o conteudo (String)
 		
+		System.out.println(usuarios.get(2)); //aqui ele informa o elemento e me retorna o item (String)
 		
+		System.out.println();
 		
+		for (int chave : usuarios.keySet()) {
+			
+			System.out.println(chave);
+			
+		}
+		
+		for (String string : usuarios.values()) {
+			System.out.println(string);
+		}
+		
+		for(Entry<Integer, String> registro: usuarios.entrySet()) {
+			System.out.print(registro.getKey() +" ==> ");
+			System.out.println(registro.getValue());
+			System.out.println("\nOu: " +registro +"\n");
+		}
 		
 		
 		
