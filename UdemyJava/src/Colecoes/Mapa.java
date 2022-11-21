23d007c7d2aa0547f4238fc045e2e03159de11a6
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2022-11-20 22:26:54 -0300

    Update Mapa.java

--- UdemyJava/src/Colecoes/Mapa.java
+++ UdemyJava/src/Colecoes/Mapa.java
@@ -11,7 +11,18 @@ public class Mapa {
 		Map<Integer, String> usuarios = new HashMap<>();
 		
 		usuarios.put(1, "roberto");
-		usuarios.put(1, "Ricardo");
+		usuarios.put(2, "Ricardo");
+		usuarios.put(3, "Emanuel");
+		usuarios.put(4, "Rebeca");
+		
+		
+		System.out.println(usuarios.size());
+		System.out.println(usuarios.isEmpty());
+		System.out.println(usuarios.keySet()); //mostra as chaves do usurio, no caso o integer
+		
+		System.out.println(usuarios.values());//mostra apenas os valores (String)
+		System.out.println(usuarios.entrySet());//mostra os valores junto com o codigo (Integer)
+		
 		
 		
 		
