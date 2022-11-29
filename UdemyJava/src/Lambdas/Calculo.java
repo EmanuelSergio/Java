775eb0cf359f38d8a6238a4a5d5a5ac0521fd75f
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2022-11-29 00:21:23 -0300

    lambdas

--- UdemyJava/src/Lambdas/Calculo.java
+++ UdemyJava/src/Lambdas/Calculo.java
@@ -1,9 +1,19 @@
 package Lambdas;
 
+@FunctionalInterface //força a ter apenas um método
 public interface Calculo {
 
 	 double executar(double a, double b);
 	
-	
+	 default String legal() {
+		 return "legal";
+	 }
+	 
+	 static String massa() {
+		 return "massa!!!";
+	 }
+	 
+	 
+	 
 	 
 }
--- UdemyJava/src/Lambdas/CalculoTeste2.java
+++ UdemyJava/src/Lambdas/CalculoTeste2.java
@@ -4,12 +4,15 @@ public class CalculoTeste2 {
 public static void main(String[] args) {
 	
 	
-	Calculo soma = (x, y) -> {	return  x + y; };
+	Calculo calc = (x, y) -> {	return  x + y; };
 	
-	System.out.println(soma.executar(2, 3));
+	System.out.println(calc.executar(2, 3));
 	
+	calc = (x, y) -> x * y; //opcao de funçao lambda, não precisa de return(ele já está implementado)
+	System.out.println(calc.executar(2, 3));
 	
-	
+	System.out.println(calc.legal());
+	System.out.println(Calculo.massa());
 	
 	
 }
