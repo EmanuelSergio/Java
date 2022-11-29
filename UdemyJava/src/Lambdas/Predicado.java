79a080fa15355f58a966130404a037dcc7e445ce
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2022-11-29 01:49:18 -0300

    lambdas

---
+++ UdemyJava/src/Lambdas/Predicado.java
@@ -0,0 +1,21 @@
+package Lambdas;
+
+import java.util.function.Predicate;
+
+public class Predicado {
+
+	public static void main(String[] args) {
+		
+		Predicate<Produto> isCaro = 
+				prod -> (prod.preco * (1-prod.desconto)) >= 750;
+		
+		Produto produto = new Produto("Note", 4000, 0.015);
+		System.out.println(isCaro.test(produto));
+		
+		
+		
+		
+	}
+	
+	
+}
---
+++ UdemyJava/src/Lambdas/Produto.java
@@ -0,0 +1,19 @@
+package Lambdas;
+
+public class Produto {
+
+	final String pacote;
+	final double preco;
+	final double desconto;
+	
+	public Produto(String pacote, double preco, double desconto) {
+		
+		this.pacote = pacote;
+		this.preco = preco;
+		this.desconto = desconto;
+	}
+	
+	
+	
+	
+}
