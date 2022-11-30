447b2240e7070ee32d9aa284794b6f3519eadb0a
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2022-11-30 03:49:24 -0300

    Update Desafio.java

--- UdemyJava/src/Lambdas/Desafio.java
+++ UdemyJava/src/Lambdas/Desafio.java
@@ -1,5 +1,6 @@
 package Lambdas;
 
+import java.text.DecimalFormat;
 import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
@@ -7,6 +8,7 @@ public class Desafio {
 public static void main(String[] args) {
 	
 	Produto p = new Produto("Ipad",3235.89,0.13);
+	//Produto p = new Produto("Ipad",2500.89,0.13);
 	
 	//double precofinal = preco *(1-desconto);
 	
@@ -29,32 +31,36 @@ public static void main(String[] args) {
 	Function<Produto, Double> valor = n -> n.preco *(1.0-n.desconto);
 	
 	
-	UnaryOperator<Double> impostoMunicipal = n -> n>=2500 ? n+(n*0.085):n;
+	UnaryOperator<Double> impostoMunicipal = n -> n>=2500 ? n+(n*0.085): n;
 	
 	/*BiFunction<Double, Double, String> resultado = 
 			(n1, n2) -> ((n1+n2)/2) >= 7 ?"Aprovado":"Reprovado";*/
 	
-	
-	System.out.println(valor.andThen(impostoMunicipal).apply(p));
-	
+	UnaryOperator<Double> frete = n -> n>=3000 ? n+100:n+50;
 	
 	
 	
+	Function<Double, String> formato = n -> new DecimalFormat(".##").format(n) ;
+	
+	Function<String, String> conceitoFinal = n -> "R$"+n ;
 	
 	
+	//Consumer<String> formato = n -> System.out.println(new DecimalFormat(".##").format(n));
 	
 	
+	//valor.andThen(impostoMunicipal).andThen(frete).andThen(conceitoFinal).apply(p);
+	
+	System.out.println(valor.andThen(impostoMunicipal).andThen(frete).andThen(formato).andThen(conceitoFinal).apply(p));
 	
 	
+	//System.out.println(valor.andThen(impostoMunicipal).andThen(frete).andThen(conceitoFinal).andThen(formato).apply(p));
 	
+		//formato.accept(valor.andThen(impostoMunicipal).andThen(frete).andThen(conceitoFinal).apply(p));
+		
+		//new DecimalFormat(".##").format(n);
 	
-	
-	
-	
-	
-	
-	
-	
+	//System.out.println( new DecimalFormat(".##").format(valor.andThen(impostoMunicipal).andThen(frete).apply(p)));
+	//System.out.println( new DecimalFormat(".##").format(valor.andThen(impostoMunicipal).andThen(frete).apply(p)));
 	
 	
 	
