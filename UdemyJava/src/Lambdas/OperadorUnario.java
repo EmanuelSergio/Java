125aa309034ad0ab99b5084b259f3f8a3e5330bf
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2022-11-30 01:18:46 -0300

    Create OperadorUnario.java

---
+++ UdemyJava/src/Lambdas/OperadorUnario.java
@@ -0,0 +1,33 @@
+package Lambdas;
+
+import java.util.function.UnaryOperator;
+
+public class OperadorUnario {
+
+	public static void main(String[] args) {
+		
+		UnaryOperator<Integer> maisDois = num -> num+2;
+		UnaryOperator<Integer> vezesDois = num -> num*2;
+		UnaryOperator<Integer> aoQuadrado = num -> num*num;
+		
+		
+		int resultado1 = maisDois
+				.andThen(vezesDois)
+				.andThen(aoQuadrado)
+				.apply(0);
+		
+		
+		
+		System.out.println(resultado1);
+		
+		int resultado2 = aoQuadrado
+				.compose(vezesDois)
+				.compose(maisDois)
+				.apply(0);//faz na sequência ao contrario
+		
+		 System.out.println(resultado2);
+		
+		
+		 
+	}
+}
