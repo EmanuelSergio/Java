42cabc9488f915694604e0c6255dbaa9e60ee2ed
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-01-08 22:28:38 -0300

    Update NovaPessoa.java

--- UdemyJava/src/JDBC/NovaPessoa.java
+++ UdemyJava/src/JDBC/NovaPessoa.java
@@ -1,10 +1,43 @@
 package JDBC;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class NovaPessoa  {
 
 	public static void main(String[] args) throws SQLException{
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Informe o nome");
+		String nome = entrada.nextLine();
+		
+		System.out.println(nome);
+		
+		Connection conexao = FabricaConexao.getConexao();
+		
+		String sql = "Insert into pessoas (nome) values"
+				+ "(?)";
+		
+		/* FORMA PARA SETAR O ID MANUALMENTE:
+		PreparedStatement stmt = conexao.prepareStatement(sql);
+		stmt.setString(1, nome);//tudo que entrar como valor aqui, sera setado como string. Impossibilitando um ataque de injeção no  banco
+		stmt.setInt(2, 100);
+		*/
+		
+		PreparedStatement stmt = conexao.prepareStatement(sql);
+		stmt.setString(1, nome);//tudo que entrar como valor aqui, sera setado como string. Impossibilitando um ataque de injeção no  banco
+		
+		
+		
+		stmt.execute();
+		
+		System.out.println("incluido");
+		
+		entrada.close();
+		
 		
 		
 		
