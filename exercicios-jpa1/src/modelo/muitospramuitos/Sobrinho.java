ee4ed5ca3c3055217905e1ebb2162851c39cb71a
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-01-14 00:26:25 -0300

    um pra muitos

--- exercicios-jpa1/src/modelo/muitospramuitos/Sobrinho.java
+++ exercicios-jpa1/src/modelo/muitospramuitos/Sobrinho.java
@@ -1,11 +1,13 @@
 package modelo.muitospramuitos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Sobrinho {
@@ -16,7 +18,45 @@ public class Sobrinho {
 	
 	private String nome;
 	
+	@ManyToMany (mappedBy = "sobrinhos")
+	private List<Tio> tios = new ArrayList<Tio>(); 
 	
-	private List<Tio> tios; 
+	public Sobrinho() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Sobrinho(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Tio> getTios() {
+		return tios;
+	}
+
+	public void setTios(List<Tio> tios) {
+		this.tios = tios;
+	}
+
+	
+	
 	
 }
