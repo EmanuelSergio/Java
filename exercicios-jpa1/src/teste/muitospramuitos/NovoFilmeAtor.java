60469ad9b8208f9e1c64f356b1313032f6cee657
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-01-17 22:49:04 -0300

    muitos pra muitos

---
+++ exercicios-jpa1/src/teste/muitospramuitos/NovoFilmeAtor.java
@@ -0,0 +1,29 @@
+package teste.muitospramuitos;
+
+import infra.DAO;
+import modelo.muitospramuitos.Ator;
+import modelo.muitospramuitos.Filme;
+
+public class NovoFilmeAtor {
+
+	public static void main(String[] args) {
+		
+		Filme filmeA = new Filme("Carros 1", 10.0);
+		Filme filmeB = new Filme("Carros 2", 8.5);
+		
+		Ator atorA = new Ator("Mcquen");
+		Ator atrizB = new Ator("Celli");
+		
+		filmeA.adicionarAtor(atorA);
+		filmeA.adicionarAtor(atrizB);
+		
+		filmeB.adicionarAtor(atorA);
+		
+		DAO<Filme> dao = new DAO<Filme>();
+		dao.incluirAtomico(filmeA);
+		
+		
+		
+	}
+	
+}
