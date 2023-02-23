3498d1ea299774bc478bc4e047cdb25bab7551ee
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-02-23 20:45:02 -0300

    Utilizando classes CSS no FX

--- exercicios-JavaFX/src/basico/Contador.java
+++ exercicios-JavaFX/src/basico/Contador.java
@@ -17,11 +17,16 @@ public class Contador extends Application{
 	public void start(Stage primaryStage) throws Exception {
 
 		Label labelTitulo = new Label("Contador");
+		labelTitulo.getStyleClass().add("titulo");
+
 		Label labelNumero = new Label("0");
-		
+		labelNumero.getStyleClass().add("numero");
+
 		Button botaoDecremento = new Button("-");
 		botaoDecremento.setOnAction(e -> {
 			contador--;
+			labelNumero.getStyleClass().remove("verde");
+			labelNumero.getStyleClass().add("vermelha");
 			labelNumero.setText(Integer.toString(contador));
 			
 		});
@@ -31,9 +36,13 @@ public class Contador extends Application{
 		Button botaoIncremento = new Button("+");
 		botaoIncremento.setOnAction(e -> {
 			contador++;
+			labelNumero.getStyleClass().remove("vermelha");
+			labelNumero.getStyleClass().add("verde");
 			labelNumero.setText(Integer.toString(contador));
 		});
 		
+		botaoIncremento.getStyleClass().add("botoes");
+		botaoDecremento.getStyleClass().add("botoes");
 		
 		HBox boxBotoes = new HBox();
 		boxBotoes.setAlignment(Pos.CENTER);
@@ -41,14 +50,23 @@ public class Contador extends Application{
 		boxBotoes.getChildren().add(botaoDecremento);
 		boxBotoes.getChildren().add(botaoIncremento);
 		 
-		VBox boxPrincipal = new VBox();
-		boxPrincipal.setSpacing(10);
-		boxPrincipal.setAlignment(Pos.CENTER);
-		boxPrincipal.getChildren().add(labelTitulo);		
-		boxPrincipal.getChildren().add(labelNumero);
-		boxPrincipal.getChildren().add(boxBotoes);
 		
-		Scene cenaPrincipal = new Scene(boxPrincipal, 400, 400);
+		
+		VBox boxConteudo = new VBox();
+		boxConteudo.getStyleClass().add("conteudo");
+		boxConteudo.setSpacing(10);
+		boxConteudo.setAlignment(Pos.CENTER);
+		boxConteudo.getChildren().add(labelTitulo);		
+		boxConteudo.getChildren().add(labelNumero);
+		boxConteudo.getChildren().add(boxBotoes);
+		
+		String caminhoDoCSS = getClass()
+				.getResource("/basico/Contador.css")
+				.toExternalForm();
+		Scene cenaPrincipal = new Scene(boxConteudo, 400, 400);
+		
+		cenaPrincipal.getStylesheets().add(caminhoDoCSS);
+		cenaPrincipal.getStylesheets().add("https://fonts.googleapis.com/css2?family=Oswald");
 		
 		primaryStage.setScene(cenaPrincipal);
 		primaryStage.show();
