ffd149689e34bb11cd14a0c9040049b0cfa0461f
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-03-03 23:10:34 -0300

    FXML

--- exercicios-JavaFX/src/layout/TesteTilePane.java
+++ exercicios-JavaFX/src/layout/TesteTilePane.java
@@ -18,7 +18,7 @@ public class TesteTilePane extends TilePane {
 			quadrados.add(new Quadrado(i *  10));
 			
 		}
-		
+		 
 		setPadding(new Insets(20));//se afasta dos outros componentes
 		setVgap(10);
 		setHgap(10);
