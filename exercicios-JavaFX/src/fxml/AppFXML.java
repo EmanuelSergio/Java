04e149c736411e17c363e11244647581e378dbba
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-03-07 00:58:30 -0300

    configurando css para fxml

--- exercicios-JavaFX/src/fxml/AppFXML.java
+++ exercicios-JavaFX/src/fxml/AppFXML.java
@@ -13,11 +13,13 @@ public class AppFXML extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
+		String arquivoCSS = getClass().getResource("/fxml/Login.css").toExternalForm();
 		URL arquivoFXML = getClass().getResource("/fxml/Login.fxml");
 		GridPane raiz = FXMLLoader.load(arquivoFXML);
-		
+		 
 		Scene cena = new Scene(raiz, 350, 400);
-		
+		cena.getStylesheets().add(arquivoCSS);
+
 		primaryStage.setResizable(false);//vai impedir de alterar o  tamanho da janela
 		primaryStage.setTitle("tela de login");
 		primaryStage.setScene(cena);
