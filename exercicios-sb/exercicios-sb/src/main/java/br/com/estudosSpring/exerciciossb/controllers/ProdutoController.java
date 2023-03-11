c86ba90df08c465d96d913df98ea6f4e36a7cbb7
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-03-10 23:54:42 -0300

    vrau

--- exercicios-sb/exercicios-sb/src/main/java/br/com/estudosSpring/exerciciossb/controllers/ProdutoController.java
+++ exercicios-sb/exercicios-sb/src/main/java/br/com/estudosSpring/exerciciossb/controllers/ProdutoController.java
@@ -1,5 +1,6 @@
 package br.com.estudosSpring.exerciciossb.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,20 +8,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.estudosSpring.exerciciossb.model.entities.Produto;
+import br.com.estudosSpring.exerciciossb.model.repositories.ProdutoRepository;
 
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
 
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	@PostMapping
 	public @ResponseBody Produto novoProduto(@RequestParam String nome) {
 		Produto produto = new Produto(nome);
-
+		produtoRepository.save(produto);
 		return produto;
 	}
 
 	
-	
+	  
 	
 	
 	
