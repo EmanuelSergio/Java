48d6e0c6d2d08b48feaca10da59cf3c14532f757
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-03-11 17:22:59 -0300

    exigindo valores para parametros

--- exercicios-sb/exercicios-sb/src/main/java/br/com/estudosSpring/exerciciossb/controllers/ProdutoController.java
+++ exercicios-sb/exercicios-sb/src/main/java/br/com/estudosSpring/exerciciossb/controllers/ProdutoController.java
@@ -1,31 +1,37 @@
 package br.com.estudosSpring.exerciciossb.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.estudosSpring.exerciciossb.model.entities.Produto;
 import br.com.estudosSpring.exerciciossb.model.repositories.ProdutoRepository;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
 
+	
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
 	@PostMapping
-	public @ResponseBody Produto novoProduto(@RequestParam String nome) {
-		Produto produto = new Produto(nome);
+	public @ResponseBody Produto novoProduto(@Valid Produto produto) { //vai exigir as regras (anotacoes) demarcadas na classe produto
 		produtoRepository.save(produto);
 		return produto;
 	}
-
-	
+ 
+	@GetMapping
+	public Iterable<Produto> obterProdutos() {
+		return produtoRepository.findAll();
+		
+	}
 	  
+	
 	
 	
 	
--- exercicios-sb/exercicios-sb/src/main/java/br/com/estudosSpring/exerciciossb/model/entities/Produto.java
+++ exercicios-sb/exercicios-sb/src/main/java/br/com/estudosSpring/exerciciossb/model/entities/Produto.java
@@ -1,10 +1,12 @@
 package br.com.estudosSpring.exerciciossb.model.entities;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Produto {
@@ -13,25 +15,31 @@ public class Produto {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
+	@NotBlank //obriga o valor a ser preenchido
 	private String nome;
+
+	@Min(value = 0)//anotacao que diz que o preco minimo é 0
+	private double preco;
+
+	@Min(0) //valor minimo
+	@Max(1)//valor maximo
+	private double desconto;
 
 	public Produto() {
 		// TODO Auto-generated constructor stub
 	}
-	
-	
-	
-	public Produto(String nome) {
+
+	public Produto(String nome, double preco, double desconto) {
 		super();
 		this.nome = nome;
+		this.desconto = desconto;
+		this.preco = preco;
 	}
-
-
-
+ 
 	public int getId() {
 		return id;
 	}
- 
+
 	public void setId(int id) {
 		this.id = id;
 	}
@@ -43,5 +51,21 @@ public class Produto {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
- 
-} 
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public double getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(double desconto) {
+		this.desconto = desconto;
+	}
+
+}
