5a703afb23b9afdab08c13765b62be0ce0140f01
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-03-12 15:21:43 -0300

    consulta paginada

--- exercicios-sb/exercicios-sb/src/main/java/br/com/estudosSpring/exerciciossb/model/repositories/ProdutoRepository.java
+++ exercicios-sb/exercicios-sb/src/main/java/br/com/estudosSpring/exerciciossb/model/repositories/ProdutoRepository.java
@@ -1,11 +1,13 @@
 package br.com.estudosSpring.exerciciossb.model.repositories;
 
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.estudosSpring.exerciciossb.model.entities.Produto;
 
 public interface ProdutoRepository 
-extends CrudRepository<Produto, Integer>{
+extends CrudRepository<Produto, Integer>, 
+PagingAndSortingRepository<Produto, Integer>{ //pagingAnd... traz metodos que fazem a paginação no json
 
 	
 	
