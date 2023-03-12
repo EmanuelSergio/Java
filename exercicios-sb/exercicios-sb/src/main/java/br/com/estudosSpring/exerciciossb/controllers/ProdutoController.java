1a9bad50e10ef6e86c95c45af9222a1dbb54a70b
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-03-12 15:01:12 -0300

    delete por id

--- exercicios-sb/exercicios-sb/src/main/java/br/com/estudosSpring/exerciciossb/controllers/ProdutoController.java
+++ exercicios-sb/exercicios-sb/src/main/java/br/com/estudosSpring/exerciciossb/controllers/ProdutoController.java
@@ -3,10 +3,11 @@ package br.com.estudosSpring.exerciciossb.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,11 +19,13 @@ import jakarta.validation.Valid;
 @RequestMapping("/api/produtos")
 public class ProdutoController {
 
+		
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
-	@PostMapping
+	//@PostMapping
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT}) //dois metodos em um 
 	public @ResponseBody Produto novoProduto(@Valid Produto produto) { //vai exigir as regras (anotacoes) demarcadas na classe produto
 		produtoRepository.save(produto);
 		return produto;
@@ -39,6 +42,21 @@ public class ProdutoController {
 		return produtoRepository.findById(id);
 		
 	}
+	
+	//metodo que altera item do banco
+//	@PutMapping
+//	public Produto alterarProduto(@Valid Produto produto) {
+//		produtoRepository.save(produto);
+//		return produto;
+//		
+//	}
+	
+	@DeleteMapping(path = "/{id}")
+	public void excluirProduto(@PathVariable int id) {
+		produtoRepository.deleteById(id);
+		
+	}
+
 	
 	
 	
